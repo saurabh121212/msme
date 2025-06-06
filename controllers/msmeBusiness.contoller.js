89f56d9bbee334df7eb7d751001c8371462121a1
Saurabh <saurabhsaini38@gmail.com>
2025-06-06 15:32:07 +0530

    update

--- controllers/msmeBusiness.contoller.js
+++ controllers/msmeBusiness.contoller.js
@@ -122,7 +122,7 @@ module.exports.getListAccordingToCategoryId = async (req, res, next) => {
     const offset = (page - 1) * limit;
 
     const business_category_id = req.params.business_category_id;
-    const is_verified =  '2'; // Default to 2 if not provided
+    const is_verified =  2; // Default to 2 if not provided
 
     const params = {
         searchParams: {},
